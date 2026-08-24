da11df5bf22a37f463eb3071a149c4abb2b57d02
Felix Harding <j.felixharding@gmail.com>
2026-08-24 14:40:07 +1000

    banner line_reader

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
 #include "main.h"
-int main()
+int main(int ac, char **av)
 {
-	char user_input[11];
+	char *user_input;
 	int user_input_len;
 	char exit[] = "exit";
 	int command_table_size;
 	int i;
 
+	printbanner();
 	static const command_t command_table[] = {
 		{"exit", exit_func},
 		{"ls", ls_func}
+		
 	};
 	command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 
 	while (1)
 	{
-		printf("WANT INPUT\n");
-		scanf("%10s", user_input);
-
-		user_input_len = 0;
-		while (user_input[user_input_len] != '\0')
+		user_input = line_reader();
+		if (user_input != NULL)
 		{
-			user_input_len++;
+			user_input_len = strlen(user_input);
+			user_input[user_input_len - 1] = '\0';
 		}
+		
+		printf("%s\n", user_input);
 		i = 0;
 		while (i < command_table_size)
 		{
--- main.h
+++ main.h
@@ -2,13 +2,18 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 typedef struct command_s{
     char *command;
     void (*function)(void);
 } command_t;
 
-void exit_func();
+//void exit_func();
 void ls_func();
+void printbanner(void);
+char *line_reader(void);
+void exit_func();
 
 #endif
