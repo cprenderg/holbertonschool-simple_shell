c82557ddb2dba5c8edb33fbaf10944bbc2b9a1ea
Felix Harding <j.felixharding@gmail.com>
2026-08-24 16:04:59 +1000

    merge main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct command_s{
     char *command;
@@ -17,6 +18,7 @@ char *line_reader(void);
 int exit_func(int argc, char **argv);
 int ls_func(int argc, char **argv);
 void quick_sort(char **array, size_t size);
+int function_search(int argc, char **argv);
 
 
 #endif
