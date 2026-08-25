75b4459a6ce1099f4d37e015f9ab21b64167cac0
Felix Harding <j.felixharding@gmail.com>
2026-08-25 13:29:30 +1000

    merge

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main(void)
 	char *user_input, *token, *temp, **argv;
 	int i, argc, command, user_input_len, command_table_size;
 
-
-	printbanner();
-	
+	printbanner();	
 	command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 	printf("This should print first\n");
 	while (1)
@@ -62,16 +60,13 @@ int main(void)
 		if (command == 0)
 		{
 			if ((function_search(argv)) == 1)
-			{
 				printf("Command execution failed\n");
-			}
 		}
-
 		free(argv);
 		free(temp);
 		free(user_input);
 	}
 	printf("(main) You said to exit\n");
-	
+
 	return (0);
 }
