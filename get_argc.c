75a1dce782cf56b9302ab19e2cc5887000e5e919
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-01 13:15:12 +1000

    changed strtok to _strtok

--- get_argc.c
+++ get_argc.c
@@ -13,11 +13,11 @@ int get_argc(char *user_input)
 	if (user_input != NULL)
 	{
 		temp = strdup(user_input);
-		token = strtok(temp, " ");
+		token = _strtok(temp, " ");
 		while (token != NULL)
 		{
 			argc++;
-			token = strtok(NULL, " ");
+			token = _strtok(NULL, " ");
 		}
 		free(temp);
 	}
