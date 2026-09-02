9c1e0855ca4141216ae284ffa3928a341bf46d4a
Felix Harding <j.felixharding@gmail.com>
2026-09-02 16:14:26 +1000

    actually pushing _unsetenv

---
+++ _unsetenv.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int _unsetenv(const char *name)
+{
+	int i = 0, len;
+	char *temp = NULL;
+
+	len = strlen(name);
+
+	if (_getenv(name) == NULL)
+	{
+		fprintf(stderr, COLOR_RED"./hsh: 1: %s: not found\n"RESET, name);
+		return (-1);
+	}
+
+	while (environ[i] != NULL)
+	{
+		if (strncmp(name, environ[i], len) == 0 && environ[i][len] == '=')
+		{
+			break;
+		}
+		i++;
+
+	}
+	while(environ[i] != NULL)
+	{
+		environ[i] = environ[i + 1];
+		i++;
+	}
+	return (0);
+}
