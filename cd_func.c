e6d2112c2a4d6b556f6949080390337a20414ea1
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-03 22:53:59 +1000

    Added condition for cd /*

--- cd_func.c
+++ cd_func.c
@@ -18,7 +18,7 @@ char *build_input(int argc, char **argv)
 	{
 		j = 0;
 		
-		while (argv[i] != NULL && argv[i][j] != '\0')
+		while (argv[i] != NULL && argv[i][j] != '\0') /* Loops through the array to get memory */
 		{
 			j++;
 		}
@@ -32,10 +32,10 @@ char *build_input(int argc, char **argv)
 
 	while (i < argc && argv[i] != NULL)
 	{
-		if (i > 0)
+		if (i > 0) /* Adds a space after each argv */
 			strcat(input, " ");
 
-		strcat(input, argv[i]);
+		strcat(input, argv[i]); /* Appending argv to input */
 		i++;
 	}
 
@@ -66,7 +66,7 @@ int cd_func(int argc, char **argv, envlist_t **env_head, int *status)
 	old_arr[2] = pwd;
 	input = build_input(4, old_arr);
 
-	if (argc < 2 || argv[1] == NULL)
+	if (argc < 2 || argv[1] == NULL) /* For input cd */
 	{
 		change_location = _getenv("HOME");
 		if (change_location == NULL || change_location[0] == '\0')
@@ -77,7 +77,7 @@ int cd_func(int argc, char **argv, envlist_t **env_head, int *status)
 			return (1);
 		}
 	}
-	else if (strcmp(argv[1], "-") == 0)
+	else if (strcmp(argv[1], "-") == 0) /* For input cd - */
 	{
 		if (previous_dir == NULL || previous_dir[0] == '\0')
 		{
@@ -119,18 +119,23 @@ int cd_func(int argc, char **argv, envlist_t **env_head, int *status)
 	{
 		free(pwd);
 		pwd = _strdup(_getenv("OLDPWD"));
-		change_location = strrchr(pwd, '/');
+		change_location = strrchr(pwd, '/'); /* find the last location of '/' */
 		*change_location = '\0';
 	}
 	else
 	{
 		char *temp;
 		
-		temp = _strdup(_getenv("OLDPWD"));
 		free(pwd);
-		pwd = malloc(strlen(temp) + strlen(change_location) + 2);
-		sprintf(pwd, "%s/%s", temp, change_location);
-		free(temp);
+		if (change_location[0] == '/')
+			pwd = _strdup(change_location);
+		else
+		{
+			temp = _strdup(_getenv("OLDPWD"));
+			pwd = malloc(strlen(temp) + strlen(change_location) + 2);
+			sprintf(pwd, "%s/%s", temp, change_location);
+			free(temp);
+		}
 	}
 	
 	pwd_arr[2] = pwd;
