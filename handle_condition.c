09d938386e9fcc79ff67dfc532ee91b8ddd54c40
Felix Harding <j.felixharding@gmail.com>
2026-09-04 10:47:03 +1000

    fixed moving too much in case of an ';'

--- handle_condition.c
+++ handle_condition.c
@@ -14,20 +14,22 @@ int handle_condition(char *input, historylist_t *history_h,
 	size_t i = 0;
 	int error = 0;
 	char *post_spec, *pre_spec;
-	char spec, specifiers[] = {'|', '&', ';'};
+	char spec, specifiers[] = "|&;";
 
 	post_spec = _strpbrk(input, specifiers);/* finding where the pipe is*/
 	spec = *post_spec;
-	post_spec += 2;
+	if (spec == ';')
+		post_spec++;
+	else
+		post_spec += 2;
 	while (input[i] != spec)
 		i++;
 	pre_spec = strndup(input, i);
-	if (pre_spec)
-		pre_spec[strlen(pre_spec)] = '\0';
+
 	if (spec == ';')
 	{
 		handle_input(pre_spec, history_h, last_status, head);
-		handle_input(post_spec, history_h, last_status, head);
+		error = handle_input(post_spec, history_h, last_status, head);
 	}
 	else if (spec == '&')
 	{
